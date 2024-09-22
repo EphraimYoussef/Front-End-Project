8493c6bc3a0fbd936fe4a90fdfce5976cb67f45f
Ephraim Youssef <ephraim.youssf@gmail.com>
2024-09-22 08:19:40 +0300

    Edit something

--- src/components/cart/CartList.jsx
+++ src/components/cart/CartList.jsx
@@ -14,7 +14,7 @@ export default function CartList() {
 
   if (amount === 0) {
     return (
-      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <div className="flex flex-col items-center justify-center min-h-screen bg-slate-100">
         <h1 className="text-2xl font-bold mb-4">Your cart is empty</h1>
         <div className="space-x-4">
           <Link href="/">
